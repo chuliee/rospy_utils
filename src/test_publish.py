90efb8dd0dd539b4238bc5facd9f0f591d5732bb
Namcheol Jung <namcheoljung@naver.com>
2021-11-25 16:56:20 +0900

    Create test_publish.py

---
+++ src/test_publish.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def main():
+    publisher = rospy.Publisher("testTopic", String, queue_size = 10)
+    rospy.init_node("testNode", anonymous=True)
+    rospy.loginfo("Initiated Test Node")
+    if not(rospy.is_shutdown()):
+        try:
+            publisher.publish("Hello, world!")
+        except KeyboardInterrupt:
+            exit()
+    else:
+        exit()
+
+if __name__ == "__main__":
+    main()
